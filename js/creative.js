2d57ab00a37b915fd2f085189ccbb7d07a0e1479
Krzysztof Olszewski <k.olszewski@deltacode.pl>
2020-04-06 12:07:01 +0200

    new

--- js/creative.js
+++ js/creative.js
@@ -6,7 +6,7 @@
     // Smooth scrolling using jQuery easing
     $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
+            var target = $(this.hash); //parto of URL after #(...)
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html, body').animate({
